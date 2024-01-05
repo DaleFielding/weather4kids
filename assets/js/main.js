bb26025acaa984cc5f453705413e7294364a31bd
Dale Fielding <dalefielding28@gmail.com>
2024-01-05 15:11:14 +0000

    Feat: update getweatherdata function to further destructure the data needed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 // const fetch = require('node-fetch'); // commented out as this stops it working in browser console.
-
 const baseUrl = "http://api.weatherapi.com/v1";
 const apiKey = "211d38f5813f4f90bfa70515240501";
 let locationName = "Bristol"; // fixed location temporarily, will be adding dynamic variables later on.
@@ -22,9 +21,29 @@ function getWeatherData() {
       // Destruct the data object
       let {
         current,
-        location
+        location,
       } = data
       // Destruct data needed from the current object 
+      let {
+        condition: weatherType,
+        feelslike_c: feelsLike,
+        precip_mm: precip,
+        temp_c: temp,
+        wind_mph: wind,
+        humidity,
+        uv,
+      } = current;
+      // Destruct data needed from the location object 
+      let {
+        lat,
+        lon,
+        country,
+        name,
+        tz_id: timezone,
+        localtime,
+      } = location;
+
+      console.log(data);
     })
     .catch((error) => {
       console.error("Error:", error);
