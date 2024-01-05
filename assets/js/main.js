b912b12b8399a7b020560bd258ba46c1cdec643b
Dale Fielding <dalefielding28@gmail.com>
2024-01-05 13:00:52 +0000

    Fix: fetch so that it displays the data correctly in the browser console

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,25 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch'); // commented out as this stops it working in browser console.
 
 const baseUrl = "http://api.weatherapi.com/v1";
 const apiKey = "211d38f5813f4f90bfa70515240501";
-const location = "Bristol"; // temporary location for testing setup, will be changing to dynamic variable.
+let locationName = "Bristol"; // fixed location temporarily, will be adding dynamic variables later on.
 
-let url = `${baseUrl}/current.json?key=${apiKey}&q=${location}`;
+let url = `${baseUrl}/current.json?key=${apiKey}&q=${locationName}`;
 
-/*Fetch the data, throw error if issue with the response, catch error and display error in console. 
- Console log data if fetch successful.
- */
+/* Fetch the data, throw an error if there's an issue with the response,
+   catch the error and display it in the console.
+   Console log data if the fetch is successful.
+*/
 fetch(url)
-  .then(response => {
+  .then((response) => {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     return response.json();
   })
-  .then(data => {
+  .then((data) => {
     console.log(data);
-
   })
-  .catch(error => {
-    console.error('Error:', error);
+  .catch((error) => {
+    console.error("Error:", error);
   });
