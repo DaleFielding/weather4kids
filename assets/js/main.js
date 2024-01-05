03e3fc9024ab6eda6055b41cda7e84d0772f3f2a
Dale Fielding <dalefielding28@gmail.com>
2024-01-05 11:06:02 +0000

    Feat: add fetch to obtain data from weatherapi or catch any errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,25 @@
+const fetch = require('node-fetch');
+
 const baseUrl = "http://api.weatherapi.com/v1";
 const apiKey = "211d38f5813f4f90bfa70515240501";
-const location = "Bristol"; // Replace with the desired location
+const location = "Bristol"; // temporary location for testing setup, will be changing to dynamic variable.
 
 let url = `${baseUrl}/current.json?key=${apiKey}&q=${location}`;
 
-console.log(url)
+/*Fetch the data, throw error if issue with the response, catch error and display error in console. 
+ Console log data if fetch successful.
+ */
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
