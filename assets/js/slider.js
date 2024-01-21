a6ca6910356cdf659651f440d4f65c329dc405c2
DaleFielding <dalefielding28@gmail.com>
2024-01-21 17:34:40 +0000

    Feat: add click event for earlier button to hide and display weather cards

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const laterButton = document.querySelector("[data-later-button]");
+  const earlierButton = document.querySelector("[data-earlier-button]");
 
   laterButton.addEventListener("click", function () {
     console.log("later");
@@ -16,10 +17,22 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
   });
+
+  earlierButton.addEventListener("click", function () {
+    console.log("earlier");
+
+    const currentCard = document.querySelector("[data-cards]:not(.d-none)");
+    const prevCard = currentCard.previousElementSibling;
+
+    if (currentCard && prevCard) {
+      currentCard.classList.add("d-none");
+      prevCard.classList.remove("d-none");
+
+      if (!prevCard.previousElementSibling) {
+        earlierButton.classList.add("d-none");
+      }
+    }
+  });
 });
 
-//   earlierButton.addEventListener("click", function () {
-//     console.log("earlier");
-//
-//   });
-// });
+// Need to make it so that the today buttons reappear after clicking the other way.
