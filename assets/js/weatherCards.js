9a5f2dc6fd1b433406e3e69db0309a86a289490d
DaleFielding <dalefielding28@gmail.com>
2024-01-22 16:26:28 +0000

    Fix: update query selectors in weathercard.js to access the correct data attributes

--- assets/js/weatherCards.js
+++ assets/js/weatherCards.js
@@ -1,6 +1,6 @@
 /** 
 setWeatherCards:
-1) Obtains the values from periodsOfTheDayAverages by object desturcturing
+1) Obtains the values from periodsOfTheDayAverages by object destructuring
 2) Query selects the image src of the appropriate data-card attribute
 3) modifys the src element with the appropriate values from periodsOfTheDayAverages
 **/
@@ -14,18 +14,18 @@ export function setWeatherCards(periodsOfTheDayAverages) {
   } = periodsOfTheDayAverages;
 
   document.querySelector(
-    "[data-still-night-card] .weather-icon"
+    "[data-card='still-night-card'] .weather-icon"
   ).src = `assets/images/weather-icons/${stillNightAverages.weatherType}.png`;
   document.querySelector(
-    "[data-morning-card] .weather-icon"
+    "[data-card='morning-card'] .weather-icon"
   ).src = `assets/images/weather-icons/${morningAverages.weatherType}.png`;
   document.querySelector(
-    "[data-afternoon-card] .weather-icon"
+    "[data-card='afternoon-card'] .weather-icon"
   ).src = `assets/images/weather-icons/${afternoonAverages.weatherType}.png`;
   document.querySelector(
-    "[data-evening-card] .weather-icon"
+    "[data-card='evening-card'] .weather-icon"
   ).src = `assets/images/weather-icons/${eveningAverages.weatherType}.png`;
   document.querySelector(
-    "[data-night-card] .weather-icon"
+    "[data-card='night-card'] .weather-icon"
   ).src = `assets/images/weather-icons/${nightAverages.weatherType}.png`;
 }
