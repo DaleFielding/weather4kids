f55519785c83438bb3b5d41f1da4bc2a20e744cc
DaleFielding <dalefielding28@gmail.com>
2024-01-22 09:33:57 +0000

    Feat: add further dynamic changes to the weather cards when today buttons are clicked

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,19 +2,44 @@ document.addEventListener("DOMContentLoaded", function () {
   const laterButton = document.querySelector("[data-later-button]");
   const earlierButton = document.querySelector("[data-earlier-button]");
 
+  const cardContainer = document.querySelector("[data-cards-container]");
+
+  const lastCard = cardContainer.lastElementChild;
+  const firstCard = cardContainer.firstElementChild;
+
   laterButton.addEventListener("click", function () {
     console.log("later");
 
     const currentCard = document.querySelector("[data-cards]:not(.d-none)");
     const nextCard = currentCard.nextElementSibling;
+    const nextNextCard = nextCard.nextElementSibling;
+    const prevCard = currentCard.previousElementSibling;
 
     if (currentCard && nextCard) {
-      currentCard.classList.add("d-none");
-      nextCard.classList.remove("d-none");
       earlierButton.classList.remove("d-none");
-
       if (!nextCard.nextElementSibling) {
         laterButton.classList.add("d-none");
+      }
+
+      currentCard.classList.add("d-none", "d-sm-block", "col-sm-3");
+      currentCard.classList.remove("col-sm-6");
+      nextCard.classList.remove("d-none");
+      nextCard.classList.add("col-12", "col-sm-6");
+
+      if (prevCard) {
+        prevCard.classList.remove("d-none", "d-sm-block", "col-sm-3");
+        prevCard.classList.add("d-none");
+      }
+
+      if (nextNextCard) {
+        // prevCard.classList.remove("d-none", "d-sm-block", "col-sm-3");
+        nextNextCard.classList.add("d-sm-block", "col-sm-3");
+      }
+
+      lastCard.classList.add("d-sm-block", "col-sm-3");
+      if (lastCard !== nextCard.nextElementSibling) {
+        // console.log(nextCard.nextElementSibling);
+        lastCard.classList.remove("d-sm-block", "col-sm-3");
       }
     }
   });
@@ -24,14 +49,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const currentCard = document.querySelector("[data-cards]:not(.d-none)");
     const prevCard = currentCard.previousElementSibling;
+    const prevPrevCard = prevCard.previousElementSibling;
+    const nextCard = currentCard.nextElementSibling;
 
     if (currentCard && prevCard) {
-      currentCard.classList.add("d-none");
-      prevCard.classList.remove("d-none");
       laterButton.classList.remove("d-none");
-
       if (!prevCard.previousElementSibling) {
         earlierButton.classList.add("d-none");
+      }
+      currentCard.classList.add("d-none", "d-sm-block", "col-sm-3");
+      currentCard.classList.remove("col-sm-6");
+      prevCard.classList.remove("d-none");
+      prevCard.classList.add("col-12", "col-sm-6");
+      if (nextCard) {
+        nextCard.classList.remove("d-none", "d-sm-block", "col-sm-3");
+        nextCard.classList.add("d-none");
+      }
+      if (prevPrevCard) {
+        prevPrevCard.classList.add("d-sm-block", "col-sm-3");
+      }
+      firstCard.classList.add("d-sm-block", "col-sm-3");
+      if (firstCard !== prevCard.previousElementSibling) {
+        firstCard.classList.remove("d-sm-block", "col-sm-3");
       }
     }
   });
